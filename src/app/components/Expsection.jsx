5232418d7c0056de7f454c9371221149dda6c9fd
Afy Wahyu <afyww18@gmail.com>
2023-12-26 04:02:27 +0700

    fix

--- src/app/components/Expsection.jsx
+++ src/app/components/Expsection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Expsection = () => {
     return (
-                <div className='grid grid-cols-3 flex items-center mx-auto text-white p-2 xl:p-5 max-w-full sm:max-w-full md:max-w-3xl xl:max-w-4xl 2xl:w-full bg-black text-center'>
+                <div className='grid grid-cols-3 items-center mx-auto text-white p-2 xl:p-5 max-w-full sm:max-w-full md:max-w-3xl xl:max-w-4xl 2xl:w-full bg-black text-center'>
                 <div className=''>
                     <h1 className='text-6xl font-extrabold'>1+</h1>
                     <span className='text-sm font-semibold'>Experience</span>
