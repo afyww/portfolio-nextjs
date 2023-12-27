c5fc52da0e1261542c2e4c491ff03404aa9b4c6d
Afy Wahyu <afyww18@gmail.com>
2023-12-28 01:49:46 +0700

    ,

--- src/app/components/Aboutcard.jsx
+++ src/app/components/Aboutcard.jsx
@@ -13,7 +13,7 @@ const Aboutcard = () => {
 
       <div class="text-white justify-center text-sm space-y-2 p-8 bg-gray-700">
         <h1 className='font-semibold text-2xl'>My Skills</h1>
-        <p className='font-light line-clamp-6'>
+        <p className='font-light'>
           I am a full stack web developer with a passion for creating interactive
           and responsive web applications. I have experience working with JavaScript,
           React, Redux, Node.js, Express, PostgreSQL, Sequelize, HTML, CSS, and Git.
@@ -21,9 +21,7 @@ const Aboutcard = () => {
           I am a team player and I am excited to work with others to create amazing applications.
           I am a full stack web developer with a passion for creating interactive
           and responsive web applications. I have experience working with JavaScript,
-          React, Redux, Node.js, Express, PostgreSQL, Sequelize, HTML, CSS, and Git.
-          I am a quick learner and I am always looking to expand my knowledge and skill set.
-          I am a team player and I am excited to work with others to create amazing applications.</p>
+          React, Redux, Node.js, Express, PostgreSQL, Sequelize, HTML, CSS, and Git.</p>
       </div>
     </div>)
 }
