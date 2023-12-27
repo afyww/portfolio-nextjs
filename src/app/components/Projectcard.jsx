19bb33430095e0f9ce1a843d42411d9399e84e1d
Afy Wahyu <afyww18@gmail.com>
2023-12-27 16:44:48 +0700

    .

--- src/app/components/Projectcard.jsx
+++ src/app/components/Projectcard.jsx
@@ -6,7 +6,7 @@ const Projectcard = () => {
     <div class="text-white justify-center text-sm space-y-5 p-10 bg-gray-700">
         <h1 className='font-semibold text-2xl'>Sistem Informasi Surat Kerja</h1>
         <Image src="/images/Dinkes-Project.png" width={600} height={600} alt='' />
-        <p className='text-left font-sm'>Sistem informasi tentang pembuatan surat kerja untuk Dinas Kesehatan
+        <p className='text-left font-light'>Sistem informasi tentang pembuatan surat kerja untuk Dinas Kesehatan
              Kota Semarang.</p>
     </div>
 </div>  )
