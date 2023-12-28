75a2c98b47f8c352122251a6a7fdd65bc8608ef0
Afy Wahyu <afyww18@gmail.com>
2023-12-28 12:41:51 +0700

    .

--- src/app/components/Projectcard.jsx
+++ src/app/components/Projectcard.jsx
@@ -12,7 +12,7 @@ const Projectcard = () => {
           <li className='text-white rounded-sm p-2 border'><a className='hover:underline hover:text-black' href="">Landing</a></li>
         </ul>
       </div>
-      <div class="grid grid-cols-2 gap-8 overflow-y-scroll h-96 text-white p-8 bg-gray-700">
+      <div class="grid xl:grid-cols-2 gap-8 overflow-y-scroll h-96 text-white p-8 bg-gray-700">
         <div className='space-y-3 rounded-sm'>
           <Image className='transition-transform ease-linear duration-300 transform hover:scale-110 rounded-sm' src="/images/Dinkes-Project.png" width={300} height={300} alt='' />
           <h1 className='font-semibold text-2xl'>Information System</h1>
