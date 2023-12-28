0bf3efccbb1f55854d2484b8416488cfa372bd5d
Afy Wahyu <afyww18@gmail.com>
2023-12-28 12:37:52 +0700

    .

--- src/app/components/Projectcard.jsx
+++ src/app/components/Projectcard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Projectcard = () => {
   return (
@@ -11,9 +12,9 @@ const Projectcard = () => {
           <li className='text-white rounded-sm p-2 border'><a className='hover:underline hover:text-black' href="">Landing</a></li>
         </ul>
       </div>
-      <div class=" grid grid-cols-2 gap-8 overflow-y-scroll h-96 text-white p-8 bg-gray-700">
+      <div class="grid grid-cols-2 gap-8 overflow-y-scroll h-96 text-white p-8 bg-gray-700">
         <div className='space-y-3 rounded-sm'>
-          <Image className='rounded-sm' src="/images/Dinkes-Project.png" width={300} height={300} alt='' />
+          <Image className='transition-transform ease-linear duration-300 transform hover:scale-110 rounded-sm' src="/images/Dinkes-Project.png" width={300} height={300} alt='' />
           <h1 className='font-semibold text-2xl'>Information System</h1>
           <p className='text-left font-light line-clamp-2 max-w-xs'>Information system work letter for Dinas Kesehatan
             Kota Semarang .Feature see the graph of work letter and employee CRUD .Build with laravel,tailwind and mysql .Information system work letter for Dinas Kesehatan
@@ -22,7 +23,7 @@ const Projectcard = () => {
           </p>
         </div>
         <div className='space-y-3 rounded-sm'>
-          <Image className='rounded-sm' src="/images/Portfolio.png" width={300} height={300} alt='' />
+          <Image className='transition-transform ease-linear duration-300 transform hover:scale-110 rounded-sm' src="/images/Portfolio.png" width={300} height={300} alt='' />
           <h1 className='font-semibold text-2xl'>Portfolio Website</h1>
           <p className='text-left font-light line-clamp-2 max-w-xs'>Portfolio website include animations .Build with nextjs,tailwind and npm library .Information system work letter for Dinas Kesehatan
             Kota Semarang .Information system work letter for Dinas Kesehatan
@@ -30,7 +31,7 @@ const Projectcard = () => {
           </p>
         </div>
         <div className='space-y-3 rounded-sm'>
-          <Image className='rounded-sm' src="/images/Dinkes-Project.png" width={300} height={300} alt='' />
+          <Image className='transition-transform ease-linear duration-300 transform hover:scale-110 rounded-sm' src="/images/Dinkes-Project.png" width={300} height={300} alt='' />
           <h1 className='font-semibold text-2xl'>Information System</h1>
           <p className='text-left font-light line-clamp-2 max-w-xs'>Information system work letter for Dinas Kesehatan
             Kota Semarang .Information system work letter for Dinas Kesehatan
@@ -40,7 +41,7 @@ const Projectcard = () => {
           </p>
         </div>
         <div className='space-y-3 rounded-sm'>
-          <Image className='rounded-sm' src="/images/Dinkes-Project.png" width={300} height={300} alt='' />
+          <Image className='transition-transform ease-linear duration-300 transform hover:scale-110 rounded-sm' src="/images/Dinkes-Project.png" width={300} height={300} alt='' />
           <h1 className='font-semibold text-2xl'>Information System</h1>
           <p className='text-left font-light line-clamp-2 max-w-xs'>Information system work letter for Dinas Kesehatan
             Kota Semarang .Information system work letter for Dinas Kesehatan
@@ -50,7 +51,7 @@ const Projectcard = () => {
           </p>
         </div>
         <div className='space-y-3 rounded-sm'>
-          <Image className='rounded-sm' src="/images/Dinkes-Project.png" width={300} height={300} alt='' />
+          <Image className='transition-transform ease-linear duration-300 transform hover:scale-110 rounded-sm' src="/images/Dinkes-Project.png" width={300} height={300} alt='' />
           <h1 className='font-semibold text-2xl'>Information System</h1>
           <p className='text-left font-light line-clamp-2 max-w-xs'>Information system work letter for Dinas Kesehatan
             Kota Semarang .Information system work letter for Dinas Kesehatan
@@ -60,7 +61,7 @@ const Projectcard = () => {
           </p>
         </div>
         <div className='space-y-3 rounded-sm'>
-          <Image className='rounded-sm' src="/images/Dinkes-Project.png" width={300} height={300} alt='' />
+          <Image className='transition-transform ease-linear duration-300 transform hover:scale-110 rounded-sm' src="/images/Dinkes-Project.png" width={300} height={300} alt='' />
           <h1 className='font-semibold text-2xl'>Information System</h1>
           <p className='text-left font-light line-clamp-2 max-w-xs'>Information system work letter for Dinas Kesehatan
             Kota Semarang .Information system work letter for Dinas Kesehatan
