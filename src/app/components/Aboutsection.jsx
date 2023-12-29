2cac14b8f4386de2c91c2b02dbb42e0417017763
Afy Wahyu <afyww18@gmail.com>
2023-12-30 05:22:41 +0700

    ...

--- src/app/components/Aboutsection.jsx
+++ src/app/components/Aboutsection.jsx
@@ -1,27 +1,29 @@
-import React from 'react';
+import React from 'react'
+import Aboutcard from './Aboutcard'
 import TypewriterComponent from 'typewriter-effect';
-import AboutCard from './AboutCard'; 
 
-const AboutSection = () => {
+
+const Aboutsection = () => {
     return (
         <section id="about">
-            <div className="grid grid-cols-1 h-screen md:h-full xl:h-screen 2xl:h-screen md:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2">
-                <div className="my-auto">
-                    <h1 className="text-6xl text-center text-white font-extrabold">
+            <div class=" grid grid-cols-1 h-screen md:h-full xl:h-screen 2xl:h-screen md:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2">
+                <div class="my-auto">
+                    <h1 class="text-6xl text-center text-white font-extrabold">
                         <TypewriterComponent 
-                            options={{ 
-                                strings: ["About"],
-                                autoStart: true,
-                                loop: true,
-                                delay: 90,
-                            }}
+                        options={{ 
+                            strings: [
+                                "About",
+                            ],
+                            autoStart: true,
+                            loop: true,
+                            delay: 90,
+                         }}
                         />
                     </h1>
                 </div>
-                <AboutCard />
+                <Aboutcard />
             </div>
-        </section>
-    );
-};
+        </section>)
+}
 
-export default AboutSection;
+export default Aboutsection
