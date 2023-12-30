e935c36e15479e77db494705a20b3a0e0061696b
Afy Wahyu <afyww18@gmail.com>
2023-12-30 18:03:53 +0700

    contactbox

--- src/app/components/Contactcard.jsx
+++ src/app/components/Contactcard.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 const ContactCard = () => {
     const [formData, setFormData] = useState({
         name: '',
-        email: '',
         message: '',
     });
 
@@ -15,14 +14,14 @@ const ContactCard = () => {
 
     const handleChange = (e) => {
         const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
+        setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
     };
 
-    const handleSubmit = async (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-    
+
         // Validate form data
-        if (!formData.name || !formData.email || !formData.message) {
+        if (!formData.name || !formData.message) {
             setNotification({
                 show: true,
                 type: 'error',
@@ -30,90 +29,79 @@ const ContactCard = () => {
             });
             return;
         }
-    
-        try {
-            const response = await fetch('/api/sendEmail', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(formData),
-            });
-    
-            if (response.ok) {
-                setNotification({
-                    show: true,
-                    type: 'success',
-                    message: 'Email sent successfully',
-                });
-            } else {
-                setNotification({
-                    show: true,
-                    type: 'error',
-                    message: 'Failed to send email',
-                });
-            }
-        } catch (error) {
+
+        // Construct the WhatsApp link
+        const phone_number = '6285786946323'; // Update with the correct phone number
+        const whatsappMessage = `Name: ${formData.name}%0AMessage: ${formData.message}`;
+        const encodedMessage = encodeURIComponent(whatsappMessage);
+        const whatsappLink = `https://wa.me/${phone_number}?text=${encodedMessage}`;
+
+        // Open the WhatsApp link in a new tab
+        window.open(whatsappLink, '_blank');
+
+        setNotification({
+            show: true,
+            type: 'success',
+            message: 'Message sent via WhatsApp',
+        });
+
+        // Clear the form data after submission
+        setFormData({
+            name: '',
+            message: '',
+        });
+
+        // Hide notification after 5 seconds (adjust the time as needed)
+        setTimeout(() => {
             setNotification({
-                show: true,
-                type: 'error',
-                message: 'Error sending email',
+                show: false,
+                type: 'success',
+                message: '',
             });
-            console.error('Error sending email:', error);
-        }
+        }, 5000);
     };
-    
 
     return (
-        <div>
+        <div className="mx-auto p-10 bg-gray-800 rounded-xl">
             <form onSubmit={handleSubmit}>
-                <div className="space-y-8">
-                    <div className="flex space-x-5">
-                        <div className="space-y-2">
-                            <label className="text-white font-light">Your Name</label>
-                            <input
-                                type="text"
-                                name="name"
-                                value={formData.name}
-                                onChange={handleChange}
-                                className="rounded-xl w-full py-2 bg-gray-600 px-2"
-                                placeholder="Name"
-                            />
-                        </div>
-                        <div className="space-y-2">
-                            <label className="text-white font-light">Your Email</label>
-                            <input
-                                type="email"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleChange}
-                                className="rounded-xl w-full py-2 bg-gray-600 px-2"
-                                placeholder="Email"
-                            />
-                        </div>
+                <div className="space-y-4">
+                    <div>
+                        <label className="text-white block">Your Name</label>
+                        <input
+                            type="text"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            className="w-full py-2 px-3 bg-gray-600 rounded-md"
+                            placeholder="Name"
+                        />
                     </div>
-                    <div className="space-y-2">
-                        <label className="text-white font-light">Your Message</label>
+                    <div>
+                        <label className="text-white block">Your Message</label>
                         <textarea
                             name="message"
                             value={formData.message}
                             onChange={handleChange}
-                            cols="40"
-                            rows="12"
+                            rows="8"
+                            className="w-full py-2 px-3 bg-gray-600 rounded-md"
                             placeholder="Write your message..."
-                            className="w-full rounded-xl py-2 px-2 bg-gray-600"
                         ></textarea>
                     </div>
                     <div>
-                        <button type="submit" className="bg-blue-800 text-white px-5 py-3 font-xl rounded-xl">
-                            Send Message
+                        <button
+                            type="submit"
+                            className="bg-green-500 text-white px-6 py-3 rounded-md hover:bg-green-600"
+                        >
+                            Send Message on WhatsApp
                         </button>
                     </div>
                 </div>
             </form>
             {notification.show && (
                 <div
-                    className={`mt-4 p-4 ${notification.type === 'success' ? 'bg-green-500' : 'bg-red-500'} text-white rounded-md`}
+                    className={`mt-4 p-4 ${
+                        notification.type === 'success' ? 'bg-green-500' : 'bg-red-500'
+                    } text-white rounded-md`}
                 >
                     {notification.message}
                 </div>
